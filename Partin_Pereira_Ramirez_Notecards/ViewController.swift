3190cd01b657078e64a838d982bde65eb9433b65
xXxYeetMasterxXx <pereirl017@tcdsb.ca>
2018-11-22 13:26:49 -0500

    removedit

--- Partin_Pereira_Ramirez_Notecards/ViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/ViewController.swift
@@ -8,7 +8,6 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("Testing")
-        print("Testing 2")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
