e36c7783cafc5da2b34e05dba5439b6ddc3e99ac
gibranrmrz <45268722+gibranrmrz@users.noreply.github.com>
2018-11-23 12:07:45 -0500

    New View Controller

---
+++ Partin_Pereira_Ramirez_Notecards/UnitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UnitViewController.swift
+//  Partin_Pereira_Ramirez_Notecards
+//
+//  Created by Period Three on 2018-11-23.
+//  Copyright © 2018 Period Three. All rights reserved.
+//
+
+import UIKit
+
+class UnitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
