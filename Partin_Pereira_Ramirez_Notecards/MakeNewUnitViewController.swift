96d3b099fe8f9a77fa5ac6822d196f67f50d43d5
xXxYeetMasterxXx <pereirl017@tcdsb.ca>
2018-11-26 13:03:38 -0500

    Merge branch 'master' of https://github.com/xXxYeetMasterxXx/Partin_Pereira_Ramirez_Notecards

--- Partin_Pereira_Ramirez_Notecards/MakeNewUnitViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/MakeNewUnitViewController.swift
@@ -3,10 +3,18 @@
 import UIKit
 
 class MakeNewUnitViewController: UIViewController {
-
+    
+    @IBOutlet weak var unitLabel: UITextField!
+    var unitMade = Unit(title: "Unit", flashcard: [])
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let safeUnit = unitLabel.text {
+            unitMade.title = safeUnit
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
 }
