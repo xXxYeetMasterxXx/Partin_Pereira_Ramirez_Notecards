37e9004a0b8a9645d7def3e2c4d0aa18cbd85cc4
xXxYeetMasterxXx <pereirl017@tcdsb.ca>
2018-11-26 12:46:28 -0500

    fuk you github

--- Partin_Pereira_Ramirez_Notecards/UnitViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/UnitViewController.swift
@@ -7,28 +7,29 @@ class UnitViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var unitTableView: UITableView!
     @IBOutlet weak var unitItem: UINavigationItem!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        unitItem.title = theSubjectArray[number].title
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return theSubjectArray[number].units.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let ourCells2 = tableView.dequeueReusableCell(withIdentifier: "UnitCell", for: indexPath)
-        
         return ourCells2
     }
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        theSubjectArray.remove(at: indexPath.row)
+        theSubjectArray[number].units.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .fade)
     }
 
-    @IBAction func unwindToUnit(unwindSegue: UIStoryboardSegue ) {
+    @IBAction func unwind2(unwindSegue: UIStoryboardSegue ) {
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        unitItem.title = theSubjectArray[number].title
+    }
+    
 }
