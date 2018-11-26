924f40205c2ac985bb247c22666dd1b10146f6b6
gibranrmrz <45268722+gibranrmrz@users.noreply.github.com>
2018-11-26 12:09:43 -0500

    Yes

--- Partin_Pereira_Ramirez_Notecards/MakeNewSubjectViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/MakeNewSubjectViewController.swift
@@ -12,6 +12,7 @@ class MakeNewSubjectViewController: UIViewController {
     
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ Partin_Pereira_Ramirez_Notecards/MakeNewUnitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MakeNewUnitViewController.swift
+//  Partin_Pereira_Ramirez_Notecards
+//
+//  Created by Period Three on 2018-11-26.
+//  Copyright © 2018 Period Three. All rights reserved.
+//
+
+import UIKit
+
+class MakeNewUnitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
