ff75dd1725d03ef5f38caf8cecbfd242270aa188
gibranrmrz <45268722+gibranrmrz@users.noreply.github.com>
2018-11-27 12:58:49 -0500

    PP

--- Partin_Pereira_Ramirez_Notecards/MakeNewFlashcardViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/MakeNewFlashcardViewController.swift
@@ -4,7 +4,17 @@ import UIKit
 
 class MakeNewFlashcardViewController: UIViewController {
 
+    
+    @IBOutlet weak var colourButton: UIButton!
+    
+   
+    @IBAction func colourButtonPressed(_ sender: Any) {
+    }
+    
+    
     var flashcardMade = Flashcard(title: "", text1: "", text2: "", colour: #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1), type: .note)
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
