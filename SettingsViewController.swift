8727d9c766841a6319e802a849b7b35de03cbde6
xXxYeetMasterxXx <pereirl017@tcdsb.ca>
2018-11-27 11:56:38 -0500

    fuk u github

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -3,13 +3,10 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
     }
 
 }
