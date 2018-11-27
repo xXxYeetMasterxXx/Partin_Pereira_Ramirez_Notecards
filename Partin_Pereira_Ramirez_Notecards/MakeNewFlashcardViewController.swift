a2d08edd3f92d71d610620ff6234feaf3ae79c68
xXxYeetMasterxXx <pereirl017@tcdsb.ca>
2018-11-27 13:17:41 -0500

    fuk u github

--- Partin_Pereira_Ramirez_Notecards/MakeNewFlashcardViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/MakeNewFlashcardViewController.swift
@@ -3,22 +3,34 @@
 import UIKit
 
 class MakeNewFlashcardViewController: UIViewController {
-
     
+    @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var colourButton: UIButton!
     
-   
-    @IBAction func colourButtonPressed(_ sender: Any) {
-    }
-    
-    
     var flashcardMade = Flashcard(title: "", text1: "", text2: "", colour: #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1), type: .note)
-    
-    
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    @IBAction func colourButtonPressed(_ sender: Any) {
+        if colourButton.backgroundColor == #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1) {
+            colourButton.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        } else if colourButton.backgroundColor == #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1) {
+            colourButton.backgroundColor = #colorLiteral(red: 1, green: 0.1857388616, blue: 0.5733950138, alpha: 1)
+        } else if colourButton.backgroundColor == #colorLiteral(red: 1, green: 0.1857388616, blue: 0.5733950138, alpha: 1) {
+            colourButton.backgroundColor = #colorLiteral(red: 0.5818830132, green: 0.2156915367, blue: 1, alpha: 1)
+        } else if colourButton.backgroundColor == #colorLiteral(red: 0.5818830132, green: 0.2156915367, blue: 1, alpha: 1) {
+            colourButton.backgroundColor = #colorLiteral(red: 0.3912315071, green: 0.7174404263, blue: 0.8626636863, alpha: 1)
+        } else if colourButton.backgroundColor == #colorLiteral(red: 0.3912315071, green: 0.7174404263, blue: 0.8626636863, alpha: 1){
+            colourButton.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        } else if colourButton.backgroundColor == #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) {
+            colourButton.backgroundColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+        }
     }
 
 }
