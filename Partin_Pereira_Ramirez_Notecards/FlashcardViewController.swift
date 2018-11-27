eb55715ef613017081e67c0a3eeb124165c7f9bb
gibranrmrz <45268722+gibranrmrz@users.noreply.github.com>
2018-11-27 12:12:37 -0500

    Yes

--- Partin_Pereira_Ramirez_Notecards/FlashcardViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/FlashcardViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class FlashcardViewController: UIViewController {
+class FlashcardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return theSubjectArray[number].units[number2].flashcards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let ourCells3 = tableView.dequeueReusableCell(withIdentifier: "FlashCardCell", for: indexPath)
+        ourCells3.textLabel?.text = theSubjectArray[number].units[number2].flashcards[indexPath.row].title
+        return ourCells3
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
