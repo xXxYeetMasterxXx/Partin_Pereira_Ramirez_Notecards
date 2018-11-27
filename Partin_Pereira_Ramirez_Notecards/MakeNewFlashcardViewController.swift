2576c4e48a456b044a8d759b63d95a7d06925b6a
xXxYeetMasterxXx <pereirl017@tcdsb.ca>
2018-11-27 12:52:55 -0500

    fuk u github

---
+++ Partin_Pereira_Ramirez_Notecards/MakeNewFlashcardViewController.swift
@@ -0,0 +1,14 @@
+//  MakeNewFlashcardViewController.swift
+
+import UIKit
+
+class MakeNewFlashcardViewController: UIViewController {
+
+    var flashcardMade = Flashcard(title: "", text1: "", text2: "", colour: #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1), type: .note)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
