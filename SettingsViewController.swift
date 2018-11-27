8b21e6a1fc19c8dba400e9425f3420374a89469e
mrCoolsville69 <partins002@tcdsb.ca>
2018-11-27 12:13:33 -0500

    s

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -4,9 +4,30 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    
+    @IBOutlet weak var studySwitch: UISwitch!
+    @IBOutlet weak var remindTextField: UITextField!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    
+    
+    
+    @IBAction func switchPressed(_ sender: Any) {
+        if studySwitch.isOn == true {
+            remindTextField.isHidden = false
+            timePicker.isHidden = false
+        } else {
+            remindTextField.isHidden = true
+            timePicker.isHidden = true
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        studySwitch.isOn = false
+        remindTextField.isHidden = true
+        timePicker.isHidden = true
     }
 
 }
