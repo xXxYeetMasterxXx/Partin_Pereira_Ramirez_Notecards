26090d617dc6ea62e43111b588cfb7c79b451698
xXxYeetMasterxXx <pereirl017@tcdsb.ca>
2018-12-03 11:50:42 -0500

    fuk u github

--- Partin_Pereira_Ramirez_Notecards/TestViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/TestViewController.swift
@@ -4,9 +4,20 @@ import UIKit
 
 class TestViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var answerButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    var testArrayOfFlashcards = theSubjectArray[number].units[number2].flashcards
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        testArrayOfFlashcards.shuffle()
+        titleLabel.text = testArrayOfFlashcards[0].title
     }
     
 }
