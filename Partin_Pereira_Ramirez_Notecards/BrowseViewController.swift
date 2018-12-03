1d03da2299b17b188bbc7b2ad0227230823c5232
gibranrmrz <45268722+gibranrmrz@users.noreply.github.com>
2018-12-03 13:15:43 -0500

    Haha yes

--- Partin_Pereira_Ramirez_Notecards/BrowseViewController.swift
+++ Partin_Pereira_Ramirez_Notecards/BrowseViewController.swift
@@ -10,6 +10,56 @@ import UIKit
 
 class BrowseViewController: UIViewController {
 
+    @IBOutlet weak var BrowseVContButton: UIButton!
+    
+    var browsedFlipped = false
+    
+    
+    
+    
+    @IBAction func nextButton(_ sender: Any) {
+        BrowseVContButton.backgroundColor = theSubjectArray[number].units[number2].flashcards[number3].colour
+        BrowseVContButton.setTitle(theSubjectArray[number].units[number2].flashcards[number3].text1, for: .normal)
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+        UIDevice.current.setValue(UIDeviceOrientation.landscapeLeft.rawValue, forKey: "orientation")
+        
+        
+        BrowseVContButton.backgroundColor = theSubjectArray[number].units[number2].flashcards[number3].colour
+        theSubjectArray[number].units[number2].flashcards.shuffle()
+        BrowseVContButton.setTitle(theSubjectArray[number].units[number2].flashcards[number3].text1, for: .normal)
+        
+        if theSubjectArray[number].units[number2].flashcards[number3].type == .note {
+            BrowseVContButton.isEnabled = false
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        UIDevice.current.setValue(UIDeviceOrientation.portrait.rawValue, forKey: "orientation")
+    }
+   
+    
+    
+    
+    @IBAction func browseVContAction(_ sender: Any) {
+        if browsedFlipped == false {
+            BrowseVContButton.setTitle(theSubjectArray[number].units[number2].flashcards[number3].text2, for: .normal)
+            UIView.transition(with: BrowseVContButton, duration: 0.5, options: .transitionFlipFromTop, animations: nil, completion: nil)
+            browsedFlipped = true
+        } else {
+            BrowseVContButton.setTitle(theSubjectArray[number].units[number2].flashcards[number3].text1, for: .normal)
+            UIView.transition(with: BrowseVContButton, duration: 0.5, options: .transitionFlipFromBottom, animations: nil, completion: nil)
+            browsedFlipped = false
+        }
+    
+    }
+    
+    
+    
     
     
     
